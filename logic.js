376c4c143675588714ac89434e3d179ae3e5bbbc
Aditya Pal <142164435+Aditya2829Pal-B@users.noreply.github.com>
2026-08-31 00:13:42 +0530

    changes made in sub_branch

--- logic.js
+++ logic.js
@@ -1,4 +1,11 @@
+const { ClientEncryption } = require("mongodb");
+
 function myFunction(url) {
     window.location.href = url;
+}
+
+
+function redirectToGitHub() {
+   myFunction('https://github.com');
 }
 
